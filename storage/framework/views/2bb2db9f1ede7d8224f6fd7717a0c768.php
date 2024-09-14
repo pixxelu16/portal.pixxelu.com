5e302f31bd693802b45fb410946d43271dc74b57
Thakur-Singh123 <kapoorthakur906@gmail.com>
2024-09-14 12:48:21 +0530

    css-changes

--- storage/framework/views/2bb2db9f1ede7d8224f6fd7717a0c768.php
+++ storage/framework/views/2bb2db9f1ede7d8224f6fd7717a0c768.php
@@ -14,36 +14,43 @@
    <div class ="search-header">
       <h2>Search Attendances</h2>
    </div>
-   <!--start four boxes studens fees-->
-   <div class="boxes-wrapper">
-      <div class="box">
-         <h3>Working Hours</h3>
-         <p><?php echo e(number_format($totalPresentHours, 2)); ?> Hrs</p>
+    <!--start four boxes studens fees-->
+    <div class="boxes-wrapper student-attendance-header">
+         <div class="box">
+            <img src="<?php echo e(url('public/admin/images/working_hours.svg')); ?>" alt="Working Hours">
+            <h3>Working Hours</h3>
+            <p><?php echo e(number_format($total_present_hours, 2)); ?> Hrs</p>
       </div>
-      <div class="box">
-         <h3>Present</h3>
-         <p><?php echo e($totalPresentDays); ?></p>
-      </div>
-      <div class="box">
-         <h3><span style="color: green;">Absent</span></h3>
-         <p><?php echo e($totalAbsentDays); ?></p>
-      </div>
-      <div class="box">
-         <h3><span style="color: green;">Leave</span></h3>
-         <p><?php echo e($totalLeaveDays); ?></p>
-      </div>
-      <div class="box">
-         <h3><span style="color: green;">Half Day </span></h3>
-         <p><?php echo e($totalHalfDay); ?></p>
-      </div>
-      <div class="box">
-         <h3><span style="color: green;">Holidays</span></h3>
-         <p><?php echo e($totalHolidays); ?></p>
-      </div>
-      <div class="box">
-         <h3><span style="color: green;">Total Days in Month</span></h3>
-         <p><?php echo e($daysInMonth); ?></p>
-      </div>
+         <div class="box">
+            <img src="<?php echo e(url('public/admin/images/present_icon.svg')); ?>" alt="Present">
+            <h3>Presents</h3>
+            <p><?php echo e($total_present_days); ?></p>
+         </div>
+         <div class="box">
+            <img src="<?php echo e(url('public/admin/images/absent_icon.svg')); ?>" alt="Absent">
+            <h3>Absent</h3>
+            <p><?php echo e($total_absent_days); ?></p>
+         </div>
+         <div class="box">
+            <img src="<?php echo e(url('public/admin/images/leave_icon.svg')); ?>" alt="Leave">
+            <h3>Leave</h3>
+            <p><?php echo e($total_leave_days); ?></p>
+         </div>
+         <div class="box">
+            <img src="<?php echo e(url('public/admin/images/half_day_leave.svg')); ?>" alt="Half Day">
+            <h3>Half Day</h3>
+            <p><?php echo e($total_half_day); ?></p>
+         </div>
+         <div class="box">
+            <img src="<?php echo e(url('public/admin/images/holiday.svg')); ?>" alt="Holidays">
+            <h3>Holidays</h3>
+            <p><?php echo e($total_holidays); ?></p>
+         </div>
+         <div class="box">
+            <img src="<?php echo e(url('public/admin/images/total_days_in_month.svg')); ?>" alt="daysInMonth">
+            <h3>Days in month</h3>
+            <p><?php echo e($daysInMonth); ?></p>
+         </div>
    </div>
    <!--end four boxes studens fees-->
 </div>
@@ -169,7 +176,7 @@
                      <?php if($attendance): ?>
                      <?php if($attendance->attendance_status == 'present'): ?>
                      <img src="<?php echo e(url('public/admin/images/present_icon.svg')); ?>" alt="Present">
-                     <p class="duration"><?php echo e($formattedDuration ?? 'N/A'); ?></p>
+                     <p class="student-attendance-duration"><?php echo e($formattedDuration ?? 'N/A'); ?></p>
                      <?php elseif($attendance->attendance_status == 'absent'): ?>
                      <img src="<?php echo e(url('public/admin/images/absent_icon.svg')); ?>" alt="Absent">
                      <?php elseif($attendance->attendance_status == 'leave'): ?>
